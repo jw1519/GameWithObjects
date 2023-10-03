1ee7a58aa9a08e600841ab4fdd603043d9f0b402
Jess Woodward <jw1519@student.aru.ac.uk>
2023-10-03 10:05:46 +0100

    added player and enemy

--- GameWithObjects/Program.cs
+++ GameWithObjects/Program.cs
@@ -2,15 +2,27 @@
 {
     internal class Program
     {
+        static int playerX = 0;
+        static int playerY = 0;
+        static int enemyX = 0;
+        static int enemyY = 0;
+
         static Random random = new Random();
         static bool isRunning = true;
         static DateTime lastUpdateTime = DateTime.Now;
         static double deltaTime;
         static double gameTimeElapsed;
+
+        static bool enemyMoveToggle = false;
+
         static void Main(string[] args)
         {
             Console.BufferHeight = Console.WindowHeight * 2;
             Console.CursorVisible = false;
+
+            PlacePlayer(0, 0);
+            PlaceEnemy(10, 10);
+
             while (isRunning)
             {
                 // Update the time
@@ -23,6 +35,19 @@
                 Thread.Sleep(100); // 100 milliseconds delay
             }
         }
+
+        private static void PlaceEnemy(int x, int y)
+        {
+            enemyX = x;
+            enemyY = y;
+        }
+
+        private static void PlacePlayer(int x, int y)
+        {
+            playerX = x;
+            playerY = y;
+        }
+
         static void Update()
         {
             // Handle input
@@ -34,6 +59,20 @@
                     case ConsoleKey.Q:
                         isRunning = false;
                         break;
+
+                    case ConsoleKey.UpArrow:
+                        playerY--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        playerY++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        playerX--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        playerX++;
+                        break;
+                      
                     default:
                         break;
                 }
@@ -42,7 +81,11 @@
         static void Draw()
         {
             Console.Clear();
-            Console.SetCursorPosition(0, 0);
+
+            Console.SetCursorPosition(playerX, playerY);
+            Console.Write("@");
+
+            Console.SetCursorPosition(0, 21);
             Console.WriteLine($"Time elapsed (ms): {Math.Round(gameTimeElapsed /1000)}");
         }
        }
